377fc3b8087d9fd8fb1d5917e8f9078568db3b08
zacharygoold <zgoold17@my.whitworth.edu>
2015-02-15 18:01:34 -0800

    Excercise 1.6

--- PhoneDirectory/Array_Based_PD.h
+++ PhoneDirectory/Array_Based_PD.h
@@ -58,19 +58,21 @@ private:
 	public:
 		Directory_Entry() {} // Default no-argument constructor
 		Directory_Entry(std::string the_name, std::string the_number) {
-			// constructor not implemented yet
+			name = the_name;
+			number = the_number;
 		}
 		std::string get_name() const {
-			return ""; // method not implemented yet
+			return name; 
 		}
 		std::string get_number() const {
-			return ""; // method not implemented yet
+			return number; 
 		}
 		void set_number(const std::string& new_number) {
-			// method not implemented yet
+			number = new_number;
 		}
 	private:
-
+		string name;
+		string number;
 	};
 
 	// Private Functions
